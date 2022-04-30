9a51bd308df21e79e57c308eb46940b65c36fcb4
Rafael Cardoso <usacb@hotmail.com>
2022-04-30 00:06:18 -0500

    Added new bulb imaged, background, alt text

--- js/scrip.js
+++ js/scrip.js
@@ -1,11 +1,21 @@
 //get html elements
 const btn = document.querySelector("#lightSwitch");
 const lightBulb = document.querySelector("img");
-const body = document.querySelector("body");
+const container = document.querySelector(".container");
 
 //add listener to switch image
 btn.addEventListener("click", e => {
     //toggle class
-    body.classList.toggle("blackBG")
+    container.classList.toggle("container-light");
     lightBulb.classList.toggle("turn-light-on");
+    btn.classList.toggle("turn-light-switch-on");
+    //toggle alt attribute
+    if(btn.classList.contains("turn-light-switch-on")){
+        lightBulb.alt = "Picture of the light bulb, its on.";
+        btn.alt = "This is a ligth switch. its on.";
+    }else{
+        lightBulb.alt = "Picture of the light bulb, its off.";
+        btn.alt = "This is a ligth switch. its off.";
+    }
+    
 })
